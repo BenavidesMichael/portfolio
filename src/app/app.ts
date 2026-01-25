3a5d17d8e8b3df4136fba5521873597f601b063a
Benavides Michael <Benavides.michael.munoz@gmail.com>
2026-01-25 17:47:44 +0100

    feat: Remove unused files and clean files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('portfolio');
-}
+export class App {}
